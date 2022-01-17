3b2afc2c888733854d2f135264cfba4e904f09d2
Dmitriy Kustov <dmitriiykustov@outlook.com>
2022-01-17 15:31:40 +0300

    Done.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function() { 
-    return "Hello, World!";
+Route::get('/', function(Request $request) {
+    $name = $request->name ?? 'Dima';
+    return "Hello, $name!";
 });
