aa99ec2a96d5cc3a90c11bc1d6fa692be0946781
Dmitriy Kustov <dmitriiykustov@outlook.com>
2022-02-24 16:53:44 +0300

    Фикс tab

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-protected $attributes = [
-    'count' => 1,
-];
+    protected $attributes = [
+        'count' => 1,
+    ];
 
     public function box()
     {
